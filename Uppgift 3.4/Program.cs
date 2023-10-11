d109d691802fad2678864be5413498368e1a29ef
HerrBergenholtz <143172207+HerrBergenholtz@users.noreply.github.com>
2023-10-11 12:39:20 +0200

    Liten fix

--- Uppgift 3.4/Program.cs
+++ Uppgift 3.4/Program.cs
@@ -6,15 +6,15 @@ namespace Uppgift_3._4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hur lång är låten som du vill spela (skriv in det såhär \" 3 30 \" ");
+            Console.WriteLine("Hur lång är låten som du vill spela (skriv in det såhär \"3 30\" ");
             string längd = Console.ReadLine();
             int mellanslagsindex = längd.IndexOf(" ");
             int minut = int.Parse(längd[..mellanslagsindex]);
             int sekund = int.Parse(längd[(mellanslagsindex + 1)..]);
 
-            int totalSekund = (minut*60 + sekund);
+            int summaSekund = (minut*60 + sekund);
 
-            if (totalSekund >165 && totalSekund< 260)
+            if (summaSekund >165 && summaSekund< 260)
             {
                 Console.WriteLine("Den får spelas");
             }
